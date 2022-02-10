879ed7b8194d462cc1c8606fe33186d6ad03dba9
nazmul162001 <nazmulprime162001@gmail.com>
2022-02-10 17:41:29 +0600

    explore Bank website with js DOM

---
+++ js/app.js
@@ -0,0 +1,17 @@
+document.getElementById('login-submit').addEventListener('click', function(){
+  // get user email 
+  const emailField = document.getElementById('user-email');
+  const userEmail = emailField.value;
+
+  // get user password 
+  const passwordField = document.getElementById('user-password');
+  const userPassword = passwordField.value;
+
+  if(userEmail == 'web@gmail.com' && userPassword == 'webboss'){
+    window.location.href = 'banking.html';
+  }
+  else{
+    alert('Please input valid info')
+  }
+
+})
